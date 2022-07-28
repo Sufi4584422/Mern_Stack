7021c47c5ccf4a178beb2b728552db0ae9b9a3d3
Muhammad Sufyan <52907510+Sufi4584422@users.noreply.github.com>
2022-07-28 21:31:25 +0500

    changes first

---
+++ myscript.js
@@ -0,0 +1,13 @@
+console.log("this is the first js page");
+var x = "2" * 8;
+console.log(x);
+var x = 9;
+var y = 4;
+
+function sum(a, b) {
+  return a + b;
+}
+console.log(sum(x, y));
+
+var mysum = sum;
+console.log(mysum);
