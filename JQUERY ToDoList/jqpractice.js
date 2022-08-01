7e0c30f4cb1f47848379c69524c54ad81e404ad9
Muhammad Sufyan <52907510+Sufi4584422@users.noreply.github.com>
2022-08-02 01:04:16 +0500

    JQUERY TOdoLIST

---
+++ JQUERY ToDoList/jqpractice.js
@@ -0,0 +1,21 @@
+$(function () {
+  $("#addbtn").click(handlebtnclick);
+  $("#todos").on("click", "li", removeMe);
+});
+
+function handlebtnclick() {
+  var newtodo = $("#newtodo").val();
+  $("#todos").append("<li>" + newtodo + "</li>");
+  if (!newtodo) {
+    $("#newtodo").addClass("error");
+    return;
+  }
+  $("#newtodo").removeClass("error");
+  $("#newtodo").val("");
+
+  // $("#todos li").click(removeMe);
+}
+
+function removeMe() {
+  $(this).remove();
+}
